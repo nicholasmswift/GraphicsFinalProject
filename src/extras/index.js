0e370574dda8835b9a6054dfd17f36f8cd4ecaad
Nick Swift <nicholasmswift@gmail.com>
2016-12-11 14:52:12 -0500

    remove old files

--- src/extras/index.js
+++
@@ -1,7 +0,0 @@
-export * from './controls/firstPersonControls';
-export * from './controls/orbitControls';
-export * from './api';
-export * from './Curve';
-export * from './Points';
-export * from './Group';
-export * from './Skybox';
